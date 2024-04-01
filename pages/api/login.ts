e7d27580a7923ae5387492538014639580145ec8
Roland Levy <contact@rolandlevy.co.uk>
2024-04-01 18:49:55 +0000

    submission working

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,6 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
@@ -12,23 +12,27 @@ type ErrorResponse = {
   error: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data | ErrorResponse>) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorResponse>
+) {
   try {
     const { username, password } = req.body || {};
 
     let loggedIn = false;
 
     if (!username || !password) {
-      return res.status(400).json({ error: 'Username and password are required' });
+      return res
+        .status(400)
+        .json({ error: "Username and password are required" });
     }
-    loggedIn = true; // this is the result of User.find({})
+    loggedIn = true; // this is the result of User.find({})s
 
-    const token = jwt.sign({ username, loggedIn }, SECRET_KEY || '');
+    const token = jwt.sign({ username, loggedIn }, SECRET_KEY || "");
 
-    res.json({ token });
-
+    res.status(200).json({ token });
   } catch (error) {
-    console.error('Error in handling login:', error);
-    res.status(500).json({ error: 'Server error' });
+    console.error("Error in handling login:", error);
+    res.status(500).json({ error: "Server error" });
   }
 }
