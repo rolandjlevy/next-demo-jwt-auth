901456109e7e06b1993d9301cc16e90c3d220f8a
Roland Levy <contact@rolandlevy.co.uk>
2024-04-02 00:09:56 +0000

    cookie working

--- pages/protected-ssr.tsx
+++ pages/protected-ssr.tsx
@@ -1,57 +1,67 @@
-import { GetServerSideProps } from 'next';
-import Link from 'next/link';
-import { parse } from 'cookie';
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { parse } from "cookie";
 import jwt from "jsonwebtoken";
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
 type Props = {
   token: string;
+  isLoggedIn: boolean;
 };
 
-export const getServerSideProps: GetServerSideProps<Props> = async ({ req, res }) => {
-
-  // Finally working!
-  const cookies = parse(req.headers.cookie || ''); // Parse cookies from the request headers
-  const token = cookies.token || '';
-
-  // TODO: fix this
-  // jwt.verify(token, SECRET_KEY || '', (err: any, decodedToken: any) => {
-  //   if (err) {
-  //     // access denied: verification error
-  //     console.log(`JWT error: ${err}`);
-  //     return res.status(401).json({ message: 'access denied' });
-  //   }});
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  req,
+}) => {
+  const cookies = parse(req.headers.cookie || "");
+  const tokenFromCookie = cookies.token || "";
+  let isLoggedIn = false;
 
   try {
-    // const host = `http://${req.headers.host}`;
-    // const fetchUrl = `${host}/api/token`;
-    // const res = await fetch(fetchUrl);
-    // const data = await res.json();
-
-    return {
-      props: {
-        token,
-      },
+    const decodedToken = jwt.verify(tokenFromCookie, SECRET_KEY || "") as {
+      loggedIn: boolean;
     };
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return {
-      props: {
-        token: '',
-      },
-    };
+    isLoggedIn = decodedToken.loggedIn;
+  } catch (err) {
+    console.error(`Access denied: verification error: ${err}`);
   }
+
+  return {
+    props: {
+      token: tokenFromCookie,
+      isLoggedIn: isLoggedIn || false,
+    },
+  };
 };
 
-const ProtectedPage: React.FC<Props> = ({ token }) => {
+const ProtectedPage: React.FC<Props> = ({ token, isLoggedIn }) => {
   return (
     <div>
       <h1>Protected Page</h1>
-      <p>Token: {token ? token : null}</p>
+      <p>isLoggedIn: {isLoggedIn ? 1 : 0}</p>
+      <p>token: {token ? token : null}</p>
       <Link href="/">Home</Link>
     </div>
   );
 };
 
 export default ProtectedPage;
+
+// try {
+// const host = `http://${req.headers.host}`;
+// const fetchUrl = `${host}/api/token`;
+// const res = await fetch(fetchUrl);
+// const data = await res.json();
+// return {
+//   props: {
+//     token,
+//   },
+// };
+// } catch (error) {
+//   console.error('Error fetching data:', error);
+//   return {
+//     props: {
+//       token: '',
+//     },
+//   };
+// }
